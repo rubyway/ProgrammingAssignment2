f5bf47b61e27e1a6aede51a2566413025a911f0a
rubyway <pszzbb@hotmail.com>
2015-05-21 22:23:36 +0800

    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Matrix inversion is usually a costly computation and there 
 ##may be some benefit to caching the inverse of a matrix rather 
-##than compute it repeatedly (there are also alternatives to 
-##matrix inversion that we will not discuss here).
+##than compute it repeatedly.
  
 
 ## This function creates a special "matrix" object that can cache 
